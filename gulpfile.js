c98b0e2871d07a812f1d0fcd9a62bf74f44a7183
Alex Lane <alex@alexlane.me>
2017-07-16 22:21:52 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+/**
+ * gulpfile.js
+ *
+ * @author Alex Lane
+ * @description Used to automate tasks to make development easier.
+ */
+// jshint esversion:6
+(function () {
+  'use strict';
+
+  const fs   = require('fs');
+  const path = require('path');
+  const gulp = require('gulp');
+  const bs   = require('browser-sync');
+
+  gulp.task('serve', [], _serve);
+
+  function _serve () {
+    let options = {
+      logLevel:      'debug',
+      logPrefix:     'Learning',
+      logConnections: true,
+      logFileChanges: true,
+      notify:         true,
+      online:         true,
+      server: {
+        baseDir:   path.resolve('src'),
+        directory: false,
+        routes: {
+          "/vendor": path.resolve('bower_components')
+        }
+      }
+    };
+
+    bs.init(options);
+
+    gulp.watch(path.normalize('src/**/*.{htm,html}'), bs.reload);
+  }
+})();
