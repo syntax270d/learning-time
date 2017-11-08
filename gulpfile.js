d7acb3393364b95eab04d3069d3af055dcf73e5c
Alexander Lane <Alexander.Lane@bcbsnc.com>
2017-11-07 21:07:02 -0500

    Added bootstrap, sass.

--- gulpfile.js
+++ gulpfile.js
@@ -13,27 +13,62 @@
   const gulp = require('gulp');
   const bs   = require('browser-sync');
 
+  gulp.task('clean', [], _clean);
   gulp.task('serve', [], _serve);
+  gulp.task('sass',  [], _sass);
 
-  function _serve () {
+  function _clean() {
+    const del = require('del');
+
+    return del([
+      path.join(__dirname, 'src/css'),
+    ]);
+  }
+
+  function _serve() {
     let options = {
-      logLevel:      'debug',
-      logPrefix:     'Learning',
-      logConnections: true,
-      logFileChanges: true,
-      notify:         true,
-      online:         true,
       server: {
         baseDir:   path.resolve('src'),
         directory: false,
         routes: {
           "/vendor": path.resolve('node_modules')
-        }
-      }
+        },
+      },
+      logLevel:      'warn',
+      logPrefix:     'Learning',
+      logConnections: true,
+      logFileChanges: true,
+      notify:         true,
+      online:         true,
     };
 
     bs.init(options);
 
-    gulp.watch(path.normalize('src/**/*.{htm,html}'), bs.reload);
+    gulp.watch(
+      path.join(__dirname + '/src/**/*.html'),
+      [], bs.reload
+    );
+
+    gulp.watch(
+      path.join(__dirname + '/src/scss/**/*.scss'),
+      ['sass'], bs.reload
+    )
+  }
+
+  function _sass() {
+    const print = require('gulp-print');
+    const sass  = require('gulp-sass');
+
+    let glob = path.join(__dirname, 'src', 'sass', 'style.scss');
+
+    gulp.src(glob)
+      // Log files
+      .pipe(print())
+
+      // Run SASS preprocessor
+      .pipe(sass().on('error', sass.logError))
+
+      // Save to src/css/style.css
+      .pipe(gulp.dest(path.join(__dirname, 'src', 'css')))
   }
 })();
